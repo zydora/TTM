503cc904e737dfadee5a36e434df13a5715a2691
DoraDong <44107656+zydora@users.noreply.github.com>
2019-03-23 19:26:31 +0800

    Add files via upload

--- Untitled.m
+++ Untitled.m
@@ -5,13 +5,13 @@ Dataset = 'MNIST';%You should change the W number nodes as same.
 [TrainImages,TestImages,TrainLabels,TestLabels] = load_dataset(Dataset);
 TrainImages = 1*TrainImages;
 TestImages = 1*TestImages;
-T = reshape(TrainImages(:,1:6000),[14,20,24,25,28]);
+T = reshape(TrainImages(:,1:6000),[14,16,20,25,42]);
 %T2 = reshape(TestImages,[28,28,10,10,100]);
 %%
 %e = 0;
-for i = 1:4
+for i = 6
     tic
-    r = [1,14,40,40,28,1];
+    r = [1,14,200,200,28,1];
     %[1,7,49,40,20,1]
 [G,ee] = TerTTSVD((T),0.01,r,2*ones(6,1),i) % ee is each error
 Reconstruct_error = tensornorm(ProTTSVD(G)-T);
